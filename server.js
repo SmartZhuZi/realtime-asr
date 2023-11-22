a8de23ddaad80f12552acc3637f386af03b3990f
SmartZhuZi <zhuruitian123@gmail.com>
2023-11-21 20:07:05 -0500

    trans to localhost dev, because of https recording

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+// 设置静态文件目录
+app.use(express.static(path.join(__dirname, 'src')));
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
